3b09c5ed7972b961e3c1160ee9a2349c40fb4723
Mjaraespejo <alexjaesp@gmail.com>
2023-03-30 19:41:45 +0100

    Update run_tgsgapcloser_onSambaAsm.sh

--- Phaw5.1 assembly generation/run_tgsgapcloser_onSambaAsm.sh
+++ Phaw5.1 assembly generation/run_tgsgapcloser_onSambaAsm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#This command run without error correction. I used racon error correction previously  and did not get any gap filled :(
-#It uses local TGS-GapCloser, which allows to use minimap argument
+#Pha5.0 : /parhyale_hawaiensis_15Feb20182_1IWOV.fa.scaffolds.fa
+#PacBio HiFi reads : m64164_211030_095407.hifi.subreads.fa
 
+``` 
 /drives/raid/AboobakerLab/software/TGS-GapCloser/TGS-GapCloser.sh \
 --scaff  /drives/ssd1/manuel/phaw/2022_analysis/asm_SAMBA_analysis/parhyale_hawaiensis_15Feb20182_1IWOV.fa.scaffolds.fa \
 --reads  /drives/raid/AboobakerLab/manuel/data_phaw_analysis/asm_gap_filling/TGS-GapCloser_anlysis/m64164_211030_095407.hifi.subreads.fa \
@@ -11,4 +12,6 @@
 --ne \
 --thread 20 \
 --tgstype pb 
+
+```
 
