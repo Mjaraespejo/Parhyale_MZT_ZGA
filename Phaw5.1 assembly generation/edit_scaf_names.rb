7582295ddf4be6a89d35f7cb75dfcc10f40f4fab
Mjaraespejo <alexjaesp@gmail.com>
2023-03-30 20:04:58 +0100

    Update edit_scaf_names.rb

--- Phaw5.1 assembly generation/edit_scaf_names.rb
+++ Phaw5.1 assembly generation/edit_scaf_names.rb
@@ -1,7 +1,7 @@
 # After running Samba and TGSGpaCloser, the output: /drives/raid/AboobakerLab/manuel/data_phaw_analysis/asm_gap_filling/TGS-GapCloser_anlysis/sambaAsm_analysis/phaw_sambaAsm.scaff_seqs contained special characters.
 # I used this script to edit those names and generate the final assembly ouput Phaw5.1
 
-```ruby
+
 count=1
 
 File.open("phaw_sambaAsm.scaff_seqs.tbl","r").each do |line|
@@ -13,4 +13,4 @@ File.open("phaw_sambaAsm.scaff_seqs.tbl","r").each do |line|
         puts line#.split("\t")[0]
     end
 end
-```
+
