e7b57cfe248d26c3a3bba8e494afe310ce7f664d
Mjaraespejo <alexjaesp@gmail.com>
2023-03-30 19:44:43 +0100

    Update run_tgsgapcloser_onSambaAsm.sh

--- Phaw5.1 assembly generation/run_tgsgapcloser_onSambaAsm.sh
+++ Phaw5.1 assembly generation/run_tgsgapcloser_onSambaAsm.sh
@@ -3,7 +3,7 @@
 #Pha5.0 : /parhyale_hawaiensis_15Feb20182_1IWOV.fa.scaffolds.fa
 #PacBio HiFi reads : m64164_211030_095407.hifi.subreads.fa
 
-```console
+```bash
 /drives/raid/AboobakerLab/software/TGS-GapCloser/TGS-GapCloser.sh \
 --scaff  /drives/ssd1/manuel/phaw/2022_analysis/asm_SAMBA_analysis/parhyale_hawaiensis_15Feb20182_1IWOV.fa.scaffolds.fa \
 --reads  /drives/raid/AboobakerLab/manuel/data_phaw_analysis/asm_gap_filling/TGS-GapCloser_anlysis/m64164_211030_095407.hifi.subreads.fa \
